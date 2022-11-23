672f3b10d2db0322e7c6edddfbc9cc80782436a2
Richard_La_Rosa <toastspeedruns@outlook.com.ar>
2022-11-23 17:15:25 -0500

    implementar las funcionalidades de cargar imagen y mostrar imagen para el administrador

--- src/main/java/com/DAA2/controller/AdministradorContoller.java
+++ src/main/java/com/DAA2/controller/AdministradorContoller.java
@@ -1,15 +1,29 @@
 package com.DAA2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.DAA2.model.Producto;
+import com.DAA2.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorContoller {
 	
+	@Autowired
+	private ProductoService productoService;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		
+		List<Producto> productos=productoService.findAll();
+		model.addAttribute("productos",productos);
+	
 		return"administrador/home";
 	}
 
