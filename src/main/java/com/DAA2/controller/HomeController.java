7b0110bb124194c03e61ded694f541abc479eb1d
Richard_La_Rosa <toastspeedruns@outlook.com.ar>
2022-11-23 21:59:56 -0500

    redirecion de carrito y obtener la cantidad , buscar el producto para añadir al carrito

--- src/main/java/com/DAA2/controller/HomeController.java
+++ src/main/java/com/DAA2/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.DAA2.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.websocket.server.PathParam;
@@ -11,8 +13,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.DAA2.model.DetalleOrden;
+import com.DAA2.model.Orden;
 import com.DAA2.model.Producto;
 import com.DAA2.service.ProductoService;
 
@@ -24,6 +30,11 @@ public class HomeController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+	
+	//datos de la orden
+	Orden orden = new Orden();
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -41,6 +52,20 @@ public class HomeController {
 		model.addAttribute("producto", producto);
 		
 		return "usuario/productohome";
+	}
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad:()", cantidad);
+		
+		return "usuario/carrito";
 	}
 
 }
